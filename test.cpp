00e03fa82182c4680d28ea3b44c23f26ca6c4677
Lucas Campos <lqcc@df.ufpe.br>
2013-09-03 18:25:46 -0300

    Added comments to the test

--- test.cpp
+++ test.cpp
@@ -7,23 +7,29 @@ double Uniform() { return rand()/(double)RAND_MAX;}
 
 int main() {
 
-	bool isOk = true;
-	isOk |= glfwInit();
-	isOk |= glfwOpenWindow( 600,600, 0,0,0,0,0,0, GLFW_WINDOW );
-	if (!isOk) {
-		std::cerr << "Could not open GLFW Window!" << std::endl;
-	}
 	int width = 600;
 	int height = 600;
 
-
+	//Setup GLFW window
+	bool isOk = true;
+	isOk |= glfwInit();
+	isOk |= glfwOpenWindow( width, height, 0,0,0,0,0,0, GLFW_WINDOW );
 	glfwSetWindowTitle("GLFW Basic Window");
+
+	//Check whether the window is open
+	if (!isOk) {
+		std::cerr << "Could not open GLFW Window!" << std::endl;
+	}
+
+	//Set openGL representation. It is a window centred in (0,0), going from (-1,-1) to (1,1)
 	glMatrixMode(GL_PROJECTION);
 	glOrtho (-1, 1, -1, 1, 0, 1);
-
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	//Create our video recorder. The video generated will be named "output.avi"
 	VideoRecorder video("output.avi", width, height);
+
+	//Runs a hundred frames of random colours
 	for (int i=0; i<100; i++) {
 		glColor3f(Uniform(), Uniform(), Uniform());
 
