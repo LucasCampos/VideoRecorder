2380f1e9a89a77212807a6a8b1cc163c9d6aa6e3
Lucas Campos <lqcc@df.ufpe.br>
2013-09-28 10:33:50 -0300

    Corrected licence headers

--- VideoRecorder.hpp
+++ VideoRecorder.hpp
@@ -8,9 +8,9 @@
  */
 
 /*
- *     This file is part of libepswriter.
+ *     This file is part of VideoRecorder.
  *
- *     libepswriter is free software; you can redistribute it and/or modify
+ *     VideoRecorder is free software; you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation; either version 3 of the License, or
  *     (at your option) any later version.
